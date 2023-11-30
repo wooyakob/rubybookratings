5ede46c48e80ff5e4134949167d9b5b4319a0ae4
wooyakob <jake_wood@mac.com>
2023-11-30 13:02:01 -0800

    add splendid book rating script

---
+++ tryruby.rb
@@ -0,0 +1,21 @@
+ratings = {
+    splendid: "a masterpiece",
+    quite_good: "enjoyed",
+    mediocre: "equal parts great and terrible",
+    quite_not_good: "notably bad",
+    abysmal: "steaming wreck"
+}
+
+
+books = {
+    "Gravity's Rainbow" => :splendid,
+    "The Great Gatsby" => :quite_good,
+    "Moby Dick" => :mediocre,
+    "A Tale of Two Cities" => :abysmal,
+    "Ulysses" => :splendid
+}
+
+# Filter and print only splendid books
+splendid_books = books.select { |book, rating| rating == :splendid}
+puts "Splendid Books:"
+splendid_books.each { |book, rating| puts "#{book} - #{ratings[rating]}" }

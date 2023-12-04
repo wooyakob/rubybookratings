b002230821cefc08d0dfa1e3b496ceeb1d8ccb1b
wooyakob <jake_wood@mac.com>
2023-12-03 17:13:47 -0800

    updating ruby script

--- tryruby.rb
+++ tryruby.rb
@@ -1,23 +1,16 @@
 ratings = {
     splendid: "a masterpiece",
-    quite_good: "enjoyed",
-    mediocre: "equal parts great and terrible",
-    quite_not_good: "notably bad",
-    abysmal: "steaming wreck"
+    decent: "enjoyed",
+    average: "equal parts great and bad",
+    meh: "notably bad",
+    yikes: "steaming wreck"
 }
-
 
 books = {
-    "Gravity's Rainbow" => :splendid,
-    "The Great Gatsby" => :quite_good,
-    "Moby Dick" => :mediocre,
-    "A Tale of Two Cities" => :abysmal,
-    "Ulysses" => :splendid
+    "Gatsby" => :splendid,
+    "Lord of the Flies" => :meh
 }
 
-# Filter and print only splendid books
 splendid_books = books.select { |book, rating| rating == :splendid}
 puts "Splendid Books:"
 splendid_books.each { |book, rating| puts "#{book} - #{ratings[rating]}" }
-
-puts splendid_books.length
